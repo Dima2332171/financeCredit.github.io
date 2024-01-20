dc7eaada817db4c9ed2d3acea9959606526f5bb8
Dima Shkoda <dimashkoda2000@gmail.com>
2024-01-20 16:57:59 +0200

    BlockResultDisplayNone

--- src/js/script.js
+++ src/js/script.js
@@ -19,15 +19,18 @@ function calculate() {
     const repaymentPeriod = parseFloat(document.getElementById('repaymentPeriodValue').value);
     const dailyRepayment = (loanAmount + (loanAmount * (interestRate / 100) * repaymentPeriod)) / repaymentPeriod;
     const totalRepayment = dailyRepayment * repaymentPeriod;
+
     if (isNaN(loanAmount) || loanAmount < 1000 || loanAmount > 50000 || isNaN(repaymentPeriod) || repaymentPeriod < 7 || repaymentPeriod > 60) {
         errorElement.textContent = "Please enter valid positive numbers for Loan Amount and Repayment Period.";
         button.disabled = true;
+        resultElement.style.display = 'none'; // Hide result block
 
     } else {
         errorElement.textContent = '';
         button.disabled = false;
+        resultElement.style.display = 'block'; // Show result block
+        resultElement.innerHTML = '<h3>Results:</h3>' +
+            '<p>Daily Repayment: $' + dailyRepayment.toFixed(2) + '</p>' +
+            '<p>Total Repayment: $' + totalRepayment.toFixed(2) + '</p>';
     }
-    resultElement.innerHTML = '<h3>Results:</h3>' +
-        '<p>Daily Repayment: $' + dailyRepayment.toFixed(2) + '</p>' +
-        '<p>Total Repayment: $' + totalRepayment.toFixed(2) + '</p>';
 }
